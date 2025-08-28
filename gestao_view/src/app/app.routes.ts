5bb64a6afdb55f02fdd002bdb95e4ddcb6a9304a
hidek <hideki-abe@hotmail.com>
2025-08-28 15:10:17 -0300

    att

--- gestao_view/src/app/app.routes.ts
+++ gestao_view/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
-import { ProducaoComponent } from './pages/producao.component/producao.component';
+import { ProducaoComponent } from './pages/producao/producao.component';
+import { Navbar } from './components/navbar/navbar';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/producao', pathMatch: 'full' },
+  { path: '', redirectTo: '/producao', pathMatch: 'full' },
   { path: 'producao', component: ProducaoComponent }
 ];
