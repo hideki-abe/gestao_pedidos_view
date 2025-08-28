9ffcb51252761a4395aecb94924d21ba4b75d1de
hidek <hideki-abe@hotmail.com>
2025-08-28 16:16:37 -0300

    att

--- gestao_view/src/app/app.ts
+++ gestao_view/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./components/navbar/navbar";
+import { TopbarComponent } from './components/topbar/topbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, TopbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
