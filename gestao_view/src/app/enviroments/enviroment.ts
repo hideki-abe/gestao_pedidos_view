a81a1ad434cf9528af89dc7359f9f9986c6b09e4
hidek <hideki-abe@hotmail.com>
2025-11-03 16:02:19 -0300

    att

--- gestao_view/src/app/enviroments/enviroment.ts
+++ gestao_view/src/app/enviroments/enviroment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://http://54.233.77.108/api', // sua URL base da API
+  apiUrl: 'http://54.233.77.108/api', // sua URL base da API
   // outras variáveis...
 };
