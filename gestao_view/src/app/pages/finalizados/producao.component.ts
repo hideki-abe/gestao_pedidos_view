463fd15c297579e26dd5e6ee018085c9338930fd
hidek <hideki-abe@hotmail.com>
2025-11-05 14:47:21 -0300

    att

---
+++ gestao_view/src/app/pages/finalizados/producao.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { TabelaProducao } from "../../components/tabela-producao/tabela-producao";
+import { TopbarComponent } from "../../components/topbar/topbar.component";
+import { Navbar } from "../../components/navbar/navbar";
+
+@Component({
+  selector: 'app-producao',
+  imports: [TabelaProducao, Navbar],
+  templateUrl: './producao.component.html',
+  styleUrl: './producao.component.scss'
+})
+export class ProducaoComponent {
+pagina: any;
+
+}
